39138fcc99621da0f0c9307d476ad925e1743958
Iain Brown <jardeth56@hotmail.com>
2021-03-25 15:28:05 +0000

    added base grpc server for dealing with cartages

---
+++ CartageServer/Services/CartageService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace CartageServer
+{
+    public class CartageService : Cartage.CartageBase
+    {
+        private readonly ILogger<CartageService> _logger;
+
+        public CartageService(ILogger<CartageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CartageReply> GetCartage(CartageRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CartageReply()
+            {
+                Message = "poop"
+            });
+        }
+    } 
+}
