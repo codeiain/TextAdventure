bbfe8195ec77dd6f4880d843970addc1b049efe7
codeiain <codeiain@outlook.com>
2021-04-10 20:31:03 +0100

    Created a new project for models

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace Models
 {
     public class Item
     {
-        
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public List<Trigger> Triggers { get; set; }
+        public string Destination { get; set; }
+        public List<Subitem> Subitems { get; set; }
+        public string Type { get; set; }
     }
 }
